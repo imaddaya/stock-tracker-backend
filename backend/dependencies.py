7bd9a3f38ee6cf6dfd89a282443fbc538f67db9e
imaddaya <142804314+imaddaya@users.noreply.github.com>
2025-07-10 16:01:56 +0000

    Updated dependencies (basically checked if anything needed to be changed , but all was clear)

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError
 from utils.jwt import decode_token
-
+#
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def get_current_user_email(token: str = Depends(oauth2_scheme)) -> str:
