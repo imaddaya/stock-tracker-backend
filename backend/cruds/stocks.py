0d8f4f3a2b2269d96a24dd27eb8cc7a0d09284cc
imaddaya <142804314+imaddaya@users.noreply.github.com>
2025-07-11 13:25:59 +0000

    updated stocks.py file

--- backend/cruds/stocks.py
+++ backend/cruds/stocks.py
@@ -11,12 +11,12 @@ def add_stock(db: Session, ticker: str, name: str, listed: bool = True):
     existing = get_stock_by_ticker(db, ticker)
     if existing:
         # Update if needed
-        existing.name = name
+        existing.stock_company_name = name
         existing.is_listed = listed
         db.commit()
         return existing
 
-    new_stock = StocksTable(symbol=ticker.upper(), name=name,is_listed=listed)
+    new_stock = StocksTable(stock_symbol=ticker.upper(), stock_company_name=name,is_listed=listed)
     db.add(new_stock)
     db.commit()
     db.refresh(new_stock)
@@ -33,6 +33,6 @@ def remove_stock(db: Session, ticker: str):
 def search_stocks(db: Session, keywords: str):
     query = f"%{keywords.lower()}%"
     return db.query(StocksTable).filter(
-        (StocksTable.symbol.ilike(query)) | 
-        (StocksTable.name.ilike(query))
+        (StocksTable.stock_symbol.ilike(query)) | 
+        (StocksTable.stock_company_name.ilike(query))
     ).all()
